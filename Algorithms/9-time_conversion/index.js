ff6778cd55351720ea4c5531bb6a7a0e3c7b04e7
vickysomtee <vickysomtee@gmail.com>
2023-05-16 01:49:18 +0100

    feat: time conversion - javascript

---
+++ Algorithms/9-time_conversion/index.js
@@ -0,0 +1,45 @@
+
+// Test case 1
+const time = "07:05:45PM";
+
+// Test case 2
+// const time =  "12:45:54PM"
+
+function timeConversion(s) {
+  let meridiemPm = {
+    "01": 13,
+    "02": 14,
+    "03": 15,
+    "04": 16,
+    "05": 17,
+    "06": 18,
+    "07": 19,
+    "08": 20,
+    "09": 21,
+    10: 22,
+    11: 23,
+    12: 12,
+  };
+
+  let meridiemAm = {
+    12: "00",
+  };
+
+  let militaryHour = "";
+
+  const meridiem = s.substring(s.length - 2, s.length);
+
+  if (meridiem === "PM") {
+    militaryHour = meridiemPm[s.substring(0, 2)];
+  } else if (meridiem === "AM" && s.substring(0, 2)) {
+    militaryHour = meridiemAm[s.substring(0, 2)];
+  } else {
+    militaryHour = s.substring(0, 2);
+  }
+
+  const militaryTime = militaryHour + s.substring(2, s.length - 2);
+
+  console.log(militaryTime);
+}
+
+timeConversion(time);
